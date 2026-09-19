2a89e7ab142def1b198905ccfab3d0fcd4265c8b
furqan0yousuf <furqan_yousuf@yahoo.com>
2026-09-19 16:30:43 -0500

    Add initial build configuration for Android app

---
+++ app/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.wordguessing.game"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.wordguessing.game"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+}
