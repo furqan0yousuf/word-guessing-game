46e618dcb58a213d6d99a5fb0266ad88aceaff9f
furqan0yousuf <furqan_yousuf@yahoo.com>
2026-09-20 18:12:52 -0500

    Add PlayerSetupActivity for player setup screen

---
+++ app/src/main/java/com/wordguessing/game/PlayerSetupActivity.kt
@@ -0,0 +1,12 @@
+package com.wordguessing.game
+
+import android.app.Activity
+import android.os.Bundle
+
+class PlayerSetupActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_player_setup)
+    }
+}
