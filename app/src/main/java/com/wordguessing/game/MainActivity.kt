3ef7220c92f85af2b96066d3f7125b0c9f310b18
furqan0yousuf <furqan_yousuf@yahoo.com>
2026-09-20 18:12:12 -0500

    Add button click listener to start PlayerSetupActivity

--- app/src/main/java/com/wordguessing/game/MainActivity.kt
+++ app/src/main/java/com/wordguessing/game/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.wordguessing.game
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 
 class MainActivity : Activity() {
@@ -8,5 +9,10 @@ class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<android.widget.Button>(R.id.localGameButton).setOnClickListener {
+            val intent = Intent(this, PlayerSetupActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
